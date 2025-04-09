d277ce421dd2d4939e8a0e4d6467b85a752b268e
franky-collab <waruif94@gmail.com>
2025-04-09 13:36:53 +0300

    third commit

--- app/src/main/java/com/tekri/harakamall/ui/screens/dashboard/DahboardScreen.kt
+++ app/src/main/java/com/tekri/harakamall/ui/screens/dashboard/DahboardScreen.kt
@@ -2,6 +2,7 @@ package com.tekri.harakamall.ui.screens.dashboard
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +15,9 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
@@ -29,20 +32,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.tekri.harakamall.R
+import com.tekri.harakamall.navigation.ROUT_ABOUT
+import com.tekri.harakamall.navigation.ROUT_HOME
+import com.tekri.harakamall.navigation.ROUT_ITEM
 import com.tekri.harakamall.ui.theme.neworange
 import com.tekri.harakamall.ui.theme.neworange1
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(navController: NavController){
-    Column(modifier = Modifier.fillMaxSize().background(neworange)
+    Column(modifier = Modifier.fillMaxSize()
+        .background(neworange)
+        .verticalScroll(rememberScrollState())
     ) {
+        TopAppBar(
+            title = { Text("Dashboard Section") },
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+                }
+            }
+
+        )
         //Box
 
         Box(){
@@ -52,14 +70,13 @@ fun DashboardScreen(navController: NavController){
                 shape = RoundedCornerShape(bottomStart = 60.dp, bottomEnd = 60.dp),
                 colors = CardDefaults.cardColors(neworange1)
             ) {
-                TopAppBar(
-                    title = { Text("Dashboard Section") },
-                    navigationIcon = {
-                        IconButton(onClick = {}) {
-                            Icon(imageVector = Icons.Default.Menu, contentDescription = "")
-                        }
-                    }
+                Text(
+                    text = "WELCOME TO MY APP , HARAKAMALL",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.ExtraBold
                 )
+
+
 
 
             }
@@ -71,7 +88,21 @@ fun DashboardScreen(navController: NavController){
                     .padding(start = 20.dp,end=20.dp).offset(y=90.dp)
             ) {
                 //CONTENTS OF CARD
+               Row {
+                   //Image
+                   Image(
+                       painter = painterResource(R.drawable.birds),
+                       contentDescription = "birds",
+                       modifier = Modifier.size(100.dp)
 
+
+                   )
+                   Spacer(modifier = Modifier.width(20.dp))
+
+                   Text(text = "a very large building or buildings containing a lot of stores and often restaurants, and usually with space around it outside for parking: Judy likes to hang out at the mall with her friends. A mall is also a street in a city or town with a lot of stores and that is closed to traffic."
+                       , fontSize = 15.sp)
+
+               }
             }
 
         }
@@ -80,7 +111,10 @@ fun DashboardScreen(navController: NavController){
         //row
         Row (modifier = Modifier.padding(start = 20.dp)){
             //card1
-            Card (modifier = Modifier.width(150.dp).height(180.dp),
+            Card (modifier = Modifier
+                .width(150.dp)
+                .height(180.dp)
+                .clickable{navController.navigate(ROUT_HOME)},
                 elevation = CardDefaults.cardElevation(10.dp)
             ){
                 Column(
@@ -97,9 +131,12 @@ fun DashboardScreen(navController: NavController){
                 }
             }
             //end of card1
-            Spacer(modifier = Modifier.width(20.dp))
+            Spacer(modifier = Modifier.width(40.dp))
             //card2
-            Card (modifier = Modifier.width(150.dp).height(180.dp),
+            Card (modifier = Modifier
+                .width(150.dp)
+                .height(180.dp)
+                .clickable{navController.navigate(ROUT_ABOUT)},
                 elevation = CardDefaults.cardElevation(10.dp)
             ){
                 Column(
@@ -120,13 +157,17 @@ fun DashboardScreen(navController: NavController){
 
         }
         //end of row
-        Spacer(modifier = Modifier.width(20.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
 
         //row
         Row (modifier = Modifier.padding(start = 20.dp)){
-            //card1
-            Card (modifier = Modifier.width(150.dp).height(180.dp),
+            //card3
+            Card (modifier = Modifier
+                .width(150.dp)
+                .height(180.dp)
+                .clickable{navController.navigate(ROUT_ABOUT)},
+
                 elevation = CardDefaults.cardElevation(10.dp)
             ){
                 Column(
@@ -142,10 +183,14 @@ fun DashboardScreen(navController: NavController){
                     Text(text = "CONTACT", fontSize = 15.sp)
                 }
             }
-            //end of card1
+            //end of card3
             Spacer(modifier = Modifier.width(20.dp))
-            //card2
-            Card (modifier = Modifier.width(150.dp).height(180.dp),
+            //card4
+            Card (modifier = Modifier
+                .width(150.dp)
+                .height(180.dp)
+                .clickable{navController.navigate(ROUT_ITEM)},
+
                 elevation = CardDefaults.cardElevation(10.dp)
             ){
                 Column(
@@ -161,7 +206,7 @@ fun DashboardScreen(navController: NavController){
                     Text(text = "PRODUCTS", fontSize = 15.sp)
                 }
             }
-            //end of card2
+            //end of card4
 
 
         }
